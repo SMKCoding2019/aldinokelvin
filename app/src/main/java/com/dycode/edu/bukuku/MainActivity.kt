62430c6f44a66717f36ad9896715e5553a89a04f
Aldino Kelvin Nanda 94 <aldino_kelvin_27rpl@student.smktelkom-mlg.sch.id>
2019-03-31 08:51:33 +0700

    init

---
+++ app/src/main/java/com/dycode/edu/bukuku/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.dycode.edu.bukuku
+
+
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
